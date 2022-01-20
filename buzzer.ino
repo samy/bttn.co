030abd4510d9d818def765494530b7ff9bef4dd5
Samy Rabih <contact@lahaut.info>
2022-01-20 17:48:00 +0100

    Ajout de l'appel HTTP

--- buzzer.ino
+++ buzzer.ino
@@ -7,40 +7,40 @@ int BUTTON_PIN = X;
 int LED_PIN = X;
 
 void setup() {
-      pinMode(BUTTON_PIN, INPUT);
+  pinMode(BUTTON_PIN, INPUT);
 
-    Serial.begin(115200);
+  Serial.begin(115200);
 
   // WiFiManager
   wifiManager.autoConnect("Buzzer");
   Serial.println("Connected.");
 }
 
-void loop() { 
-    if (HIGH == digitalRead(BUTTON_PIN)) {
-        // On garde la LED allumée
-        digitalWrite(LED_PIN,HIGH);
-        
-        // Si le bouton était déja pressé on ne va pas plus loin
-        if (isButtonPressed) {
-            return;
-        }
-        
-        //Si on arrive là, c'est la première fois qu'on détecte l'appui
-        sendHTTPCall();
-        isButtonPressed = true;
-    } else {
-        // Si le bouton était déja remonté, on ne fait rien
-        if (!isButtonPressed) {
-            return;
-        }
-        
-        //Sinon, on éteint la LED
-        digitalWrite(LED_PIN,LOW); 
-        isButtonPressed = false;
+void loop() {
+  if (HIGH == digitalRead(BUTTON_PIN)) {
+    // On garde la LED allumée
+    digitalWrite(LED_PIN, HIGH);
+
+    // Si le bouton était déja pressé on ne va pas plus loin
+    if (isButtonPressed) {
+      return;
     }
+
+    //Si on arrive là, c'est la première fois qu'on détecte l'appui
+    sendHTTPCall();
+    isButtonPressed = true;
+  } else {
+    // Si le bouton était déja remonté, on ne fait rien
+    if (!isButtonPressed) {
+      return;
+    }
+
+    //Sinon, on éteint la LED
+    digitalWrite(LED_PIN, LOW);
+    isButtonPressed = false;
+  }
 }
 
 void sendHTTPCall() {
+  Serial.println("I'm not a HTTP call");
 }
-
